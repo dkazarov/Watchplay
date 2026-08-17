27aabc85c9744cf7eb05678434458711b7e4c2e3
Dmytro Kazarov <d.kazarov1361@gmail.com>
2026-08-17 23:18:35 +0300

    global: prepare to new project

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,15 +1,3 @@
-// @ts-check
-import { toggleSearch, clickToAnotherPlace, pushButton } from './components/search';
-import { burger, clickOnOutside } from './components/burger';
-
 document.addEventListener('DOMContentLoaded', () => {
-  // Burger Menu
-  burger();
-  clickOnOutside();
 
-  //Search
-  toggleSearch();
-  clickToAnotherPlace();
-  pushButton('Enter');
-  pushButton('Escape');
 });
